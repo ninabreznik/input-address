959b9ff1dc323b865366efbae8a6c9557c695270
ninabreznik <ninabreznik@gmail.com>
2018-08-04 17:36:14 +0100

    Add type to params

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ var csjs = require('csjs-inject')
 
 module.exports = displayAddressInput
 
-function displayAddressInput({name, theme: {classes: css}}) {
-  var input = bel`<input class=${css.inputField} placeholder='0x6e2...'>`
+function displayAddressInput({theme: {classes: css}, type}) {
   return bel`
-    <div class=${css.inputContainer}>
-      <div class=${css.inputTitle}>${name}</div>
-      <div class=${css.inputFields}>
+      <div class=${css.addressField}>
         <div class=${css.keyField}><i class="${css.icon} fa fa-key"></i></div>
-        ${input}
-      </div>
-    </div>`
+        <input class=${css.inputField} placeholder='0x6e2...'>
+      </div>`
 }

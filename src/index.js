2eee72d05fd44e86836bbccc2085d4bedcc39b97
ninabreznik <ninabreznik@gmail.com>
2018-09-16 04:25:29 +0100

    Remove key icon

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ module.exports = displayAddressInput
 function displayAddressInput({theme: {classes: css}, type}) {
   return bel`
       <div class=${css.addressField}>
-        <div class=${css.keyField}><i class="${css.icon} fa fa-key"></i></div>
         <input class=${css.inputField} placeholder='0x6e2...'>
       </div>`
 }
